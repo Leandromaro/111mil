aec835ef7344296f04fb7cfd48331e1379a5a8a5
unknown <armandogabrielluna49@hotmail.com>
2018-08-30 20:33:15 -0300

    chau

--- Guia5.1ColasyPilas/src/guia5/pkg1colasypilas/Ej4Razon.java
+++
@@ -1,5 +0,0 @@
-package guia5.pkg1colasypilas;
-
-public enum Ej4Razon {
-    extraccion,deposito
-}
--- Guia5.1ColasyPilas/src/guia5/pkg1colasypilas/Ej4TipoPersona.java
+++
@@ -1,6 +0,0 @@
-package guia5.pkg1colasypilas;
-
-public enum Ej4TipoPersona {
-    normal,especial
-}
-
